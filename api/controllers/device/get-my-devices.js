e33987fe0a1b2341bc6f31281ea722112dccee09
Federico Mello <fedemello@gmail.com>
2020-08-25 20:16:11 -0300

    todos los cambios importantes hasta hoy

--- api/controllers/device/get-my-devices.js
+++ api/controllers/device/get-my-devices.js
@@ -11,9 +11,9 @@
  * Get my devices.
  */
 module.exports = async function getMyDevices(req, res) {
-  let userId = req.session.userId
-  let query = await User.find({where: {id : userId}}).populate('managing')
-  let subQuery = query[0]
-  let devices = subQuery['managing']
+  let userId = req.session.userId;
+  let query = await User.find({where: {id : userId}}).populate('managing');
+  let subQuery = query[0];
+  let devices = subQuery['managing'];
   return res.json(devices);
 };
--- api/controllers/device/suscribe.js
+++ api/controllers/device/suscribe.js
@@ -8,21 +8,30 @@ module.exports = {
 
 
   inputs: {
-
+    deviceId: {
+      description: 'id of a device that user is managing',
+      type: 'string',
+      required: true
+    },
+    deviceStatus: {
+      description: 'a json object containing a status report',
+      type: 'json',
+    }
   },
 
 
   exits: {
-
+    success: {description: 'Device successfully suscribed!'},
+    badId: {description: 'Device ID not found'},
+    badStatusReport: {description: 'Bad device status object. '+
+    'The received status report object does not match with '+
+    'expected for the device'}
   },
 
-
   fn: async function (inputs) {
-
-    // All done.
+    //var devToSuscribe = 
     return;
 
   }
-
 
 };
