fff77b6ec1f3c449ab910f9808c6bb0b9af770c1
Hernan <hpereyradaluz@gmail.com>
2020-10-31 19:59:52 -0300

    security.js

--- config/security.js
+++ config/security.js
@@ -28,7 +28,10 @@ module.exports.security = {
 
   cors: {
     allRoutes: true,
-    allowOrigins: ["http://localhost:3000"],
+    allowOrigins: [
+      "http://localhost:3000",
+      "http://rocky-peak-50757.herokuapp.com",
+    ],
     allowCredentials: true,
   },
 
