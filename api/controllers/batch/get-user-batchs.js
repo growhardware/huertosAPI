244eeb60765645be28a0eceb0eb22380c900d862
IA3 <ingenieriart3@gmail.com>
2025-01-22 22:35:30 -0300

    fixing controllers method

--- api/controllers/batch/get-user-batchs.js
+++ api/controllers/batch/get-user-batchs.js
@@ -3,41 +3,17 @@ module.exports = {
 
   description: "Get batchs managed by the user logged in",
 
-  // inputs: {
-  //   userId: {
-  //     description: "The ID of the user to look up.",
-  //     // By declaring a numeric example, Sails will automatically respond with `res.badRequest`
-  //     // if the `userId` parameter is not a number.
-  //     type: "string",
-  //     // By making the `userId` parameter required, Sails will automatically respond with
-  //     // `res.badRequest` if it's left out.
-  //     required: true,
-  //   },
-  // },
-
-  // exits: {
-  //   success: {
-  //     responseType: "view",
-  //     viewTemplatePath: "pages/welcome",
-  //   },
-  //   notFound: {
-  //     description: "No user with the specified ID was found in the database.",
-  //     responseType: "notFound",
-  //   },
-  // },
-
   fn: async function () {
-    // console.log("req", this.req.session.userId);
-    const userId = this.req.session.userId;
-    const user = await User.findOne({ id: userId }).populate("managing");
-    const batchIds = user["managing"].map((d) => d.id);
-    let batchs = await Batch.find({ id: batchIds }).populate("history");
-
-    if (this.req.isSocket) {
-      const ids = batchs.map((d) => d.id);
-      Batch.subscribe(this.req, ids);
+    try {
+      const userId = this.req.session.userId;
+      // Query the Batch model to find all posts created by the specified user
+      const batchs = await Batch.find({
+        user: userId,
+      });
+      // Return the batchs
+      return batchs;
+    } catch (err) {
+      return err;
     }
-
-    return batchs;
   },
 };
--- api/controllers/strain/get-user-strains.js
+++ api/controllers/strain/get-user-strains.js
@@ -3,41 +3,17 @@ module.exports = {
 
   description: "Get strains managed by the user logged in",
 
-  // inputs: {
-  //   userId: {
-  //     description: "The ID of the user to look up.",
-  //     // By declaring a numeric example, Sails will automatically respond with `res.badRequest`
-  //     // if the `userId` parameter is not a number.
-  //     type: "string",
-  //     // By making the `userId` parameter required, Sails will automatically respond with
-  //     // `res.badRequest` if it's left out.
-  //     required: true,
-  //   },
-  // },
-
-  // exits: {
-  //   success: {
-  //     responseType: "view",
-  //     viewTemplatePath: "pages/welcome",
-  //   },
-  //   notFound: {
-  //     description: "No user with the specified ID was found in the database.",
-  //     responseType: "notFound",
-  //   },
-  // },
-
   fn: async function () {
-    // console.log("req", this.req.session.userId);
-    const userId = this.req.session.userId;
-    const user = await User.findOne({ id: userId }).populate("managing");
-    const strainIds = user["managing"].map((d) => d.id);
-    let strains = await Strain.find({ id: strainIds }).populate("history");
-
-    if (this.req.isSocket) {
-      const ids = strains.map((d) => d.id);
-      Strain.subscribe(this.req, ids);
+    try {
+      const userId = this.req.session.userId;
+      // Query the Strain model to find all posts created by the specified user
+      const strains = await Strain.find({
+        user: userId,
+      });
+      // Return the strains
+      return strains;
+    } catch (err) {
+      return err;
     }
-
-    return strains;
   },
 };
